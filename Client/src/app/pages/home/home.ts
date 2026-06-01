abb9c4837fdb6bb012f4b31b5b8fcfe6a7073e50
Andrea-Agosta <andrea.agosta85@gmail.com>
2026-06-01 15:11:02 +0200

    Add featured-product section

--- Client/src/app/pages/home/home.ts
+++ Client/src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { HeroSection } from "./sections/hero/hero";
 import { TrustIndicator } from "./sections/trust-indicator/trust-indicator";
 import { Categories } from './sections/categories/categories';
+import { FeaturedProducts } from './sections/featured-products/featured-products';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, TrustIndicator, Categories],
+  imports: [HeroSection, TrustIndicator, Categories, FeaturedProducts],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
