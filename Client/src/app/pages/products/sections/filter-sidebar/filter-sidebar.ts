4e9a645d8b4a2c65db4c5c01fbf9a8b75464dc77
Andrea-Agosta <andrea.agosta85@gmail.com>
2026-06-05 13:54:04 +0200

    add products-grid section

--- Client/src/app/pages/products/sections/filter-sidebar/filter-sidebar.ts
+++ Client/src/app/pages/products/sections/filter-sidebar/filter-sidebar.ts
@@ -1,25 +1,54 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { LucideFunnel, LucideStar } from "@lucide/angular";
 
-import { ProductsFiltersStore } from '../../state/products-filters.store';
+import { CategoryService } from '../../../../services/category.service';
+import { Category } from '../../../../services/category.model';
 import { Checkbox } from '../../../../components/checkbox/checkbox';
 import { Slider } from '../../../../components/slider/slider';
 import { Button } from '../../../../components/button/button';
 
 @Component({
   selector: 'app-filter-sidebar',
-  imports: [Checkbox, Slider, FormsModule, LucideFunnel, LucideStar, Button],
+  imports: [Checkbox, FormsModule, LucideFunnel, Slider, LucideStar, Button],
   templateUrl: './filter-sidebar.html',
-  styleUrl: './filter-sidebar.scss'
+  styleUrl: './filter-sidebar.scss',
 })
 export class FilterSidebar implements OnInit {
+  @Input() rating: number = 0;
+  private categoryService = inject(CategoryService);
+  selectedCategories = signal<string[]>([]);
+  categories = signal<Category[]>([]);
+  isCategoryLoading = signal<boolean>(true);
+  error = signal<any>(null);
+  currentPrice = signal<number>(0);
+  priceRange = signal<[number, number]>([0, 2000]);
 
-  store = inject(ProductsFiltersStore);
+  updatePrice(newValue: number) {
+    this.currentPrice.set(newValue);
+    this.priceRange.set([newValue, 2000])
+  }
 
-  ngOnInit() {
-    if (!this.store.categories().length) {
-      this.store.loadCategories();
+  toggleCategory(isChecked: boolean, categoryName: string) {
+    if (isChecked) {
+      this.selectedCategories.update(current => [...current, categoryName]);
+    } else {
+      this.selectedCategories.update(current => current.filter(name => name !== categoryName));
     }
+  }
+  
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: (err) => {
+        this.error.set(err);
+        this.isCategoryLoading.set(false);
+      },
+      complete: () => {
+        this.isCategoryLoading.set(false);
+      }
+    });
   }
 }
