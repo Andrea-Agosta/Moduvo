801b2a5847957bc5c250ae663502c97749d2ff6c
Andrea-Agosta <andrea.agosta85@gmail.com>
2026-06-11 13:21:08 +0200

    add product-detail page

--- Client/src/app/pages/product-detail/product-detail.ts
+++ Client/src/app/pages/product-detail/product-detail.ts
@@ -1,26 +1,30 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { ImageGallery } from "./sections/image-gallery/image-gallery";
 import { Product } from '../../services/product.model';
 import { ProductService } from '../../services/product.service';
+import { LucideArrowLeft } from '@lucide/angular';
+import { ProductInfo } from "./sections/product-info/product-info";
 
 @Component({
   selector: 'app-product-detail',
-  imports: [ImageGallery],
+  imports: [ImageGallery, RouterLink, LucideArrowLeft, ProductInfo],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.scss',
 })
 export class ProductDetail implements OnInit {
   private productService = inject(ProductService);
-  products = signal<Product[]>([]);
+  product = signal<Product | undefined>(undefined);
   isProductLoading = signal<boolean>(true);
   error = signal<any>(null);
   childName = "products"
+  id = input.required<string>();
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe({
+    this.productService.getProductById(this.id()).subscribe({
       next: (data) => {
-        this.products.set(data);
+        this.product.set(data);
       },
       error: (err) => {
         this.error.set(err);
