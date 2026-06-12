72c38febc1791d7f670f5ae97ec70a67746192ba
Andrea-Agosta <andrea.agosta85@gmail.com>
2026-06-12 17:53:46 +0200

    add deals page

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home'
 import { Products } from './pages/products/products';
 import { ProductDetail } from './pages/product-detail/product-detail';
+import { Deals } from './pages/deals/deals';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'products', component: Products },
-    { path: 'product/:id', component: ProductDetail },
+    { path: 'products/deals', component: Deals },
+    { path: 'products/:id', component: ProductDetail },
 ];
