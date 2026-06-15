ef0bcd7d92b99bc537d5e547a25c4cfd9d788f4d
Andrea-Agosta <andrea.agosta85@gmail.com>
2026-06-15 14:46:09 +0200

    refactoring products-grid section with new card component

--- Client/src/app/pages/products/sections/products-grid/products-grid.ts
+++ Client/src/app/pages/products/sections/products-grid/products-grid.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FormsModule } from '@angular/forms'
-import { RouterLink } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { LucideHeart, LucideShoppingCart, LucideStar } from "@lucide/angular";
+import { LucideHeart, LucideShoppingCart } from "@lucide/angular";
 
 import { ProductService } from '../../../../services/product/product.service';
 import { Product } from '../../../../services/product/product.model';
 import { Button } from "../../../../components/button/button";
 import { addProduct } from '../../../../store/cart/cart.actions';
+import { Card } from "../../../../components/card/card";
 
 interface SelectOption {
   value: string;
@@ -20,7 +20,7 @@ interface SelectOption {
 
 @Component({
   selector: 'app-products-grid',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, RouterLink, FormsModule, Button, LucideHeart, LucideStar, LucideShoppingCart],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, FormsModule, Button, LucideHeart, LucideShoppingCart, Card],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
@@ -38,9 +38,14 @@ export class ProductsGrid implements OnInit {
     { value: 'rating', label: 'Highest Rated' },
     { value: 'newest', label: 'Newest' }
   ];
+  isFavorite = false
 
   ngOnInit(): void {
     this.productService.loadProducts();
+  }
+
+  addFavorite(){
+    this.isFavorite = !this.isFavorite
   }
 
   addToCart(product: Product) {
