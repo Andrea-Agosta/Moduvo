f07e2f6dfaa3d969dab0bf57bcea60b59a337bdf
Andrea-Agosta <andrea.agosta85@gmail.com>
2026-06-16 14:31:08 +0200

    add contact section

---
+++ Client/src/app/pages/about/sections/contacts/contacts.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { Button } from "../../../../components/ui/button/button";
+
+@Component({
+  selector: 'app-contacts',
+  imports: [Button],
+  templateUrl: './contacts.html',
+  styleUrl: './contacts.scss',
+})
+export class Contacts {
+
+}
